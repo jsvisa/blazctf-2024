8a53b5f4b5dd49066fd579731f0878dcb6d117f3
jsvisa <delweng@gmail.com>
2024-11-29 15:16:08 +0800

    ctf-server: easy ticket support mock submit score
    Signed-off-by: jsvisa <delweng@gmail.com>

--- infrastructure/paradigmctf.py/ctf_server/easy_ticket.py
+++ infrastructure/paradigmctf.py/ctf_server/easy_ticket.py
@@ -1,6 +1,5 @@
 import logging
-import sys
-from typing import Dict
+from typing import Any
 import hashlib
 import uuid
 
@@ -8,16 +7,18 @@ from fastapi import FastAPI
 
 from .types import CheckTicketRequest
 
-import uuid
-
 app = FastAPI()
 
+
 def string_to_md5_guid(s):
-    byte_string = s.encode('utf-8')
+    byte_string = s.encode("utf-8")
     md5_hash = hashlib.md5(byte_string).hexdigest()
-    guid_format = '{}-{}-{}-{}-{}'.format(md5_hash[:8], md5_hash[8:12], md5_hash[12:16], md5_hash[16:20], md5_hash[20:])
+    guid_format = "{}-{}-{}-{}-{}".format(
+        md5_hash[:8], md5_hash[8:12], md5_hash[12:16], md5_hash[16:20], md5_hash[20:]
+    )
 
     return guid_format
+
 
 @app.post("/api/internal/check-ticket")
 def create_instance(args: CheckTicketRequest):
@@ -31,5 +32,12 @@ def create_instance(args: CheckTicketRequest):
         "ticket": {
             "challengeId": c_id,
             "teamId": f"{c_id}-{guid}",
-        }
+        },
     }
+
+
+@app.post("/api/internal/submit")
+def submit_score(args: Any):
+    print(f"submitting score: {args}")
+
+    return {"ok": True}
