b3db1d245828a40ae00e9479b48ca933134a1c82
jsvisa <delweng@gmail.com>
2024-11-29 21:03:13 +0800

    infra/gen: check ip addr
    Signed-off-by: jsvisa <delweng@gmail.com>

--- gen_docker_compose.py
+++ gen_docker_compose.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
+import re
 import os
 import sys
 import yaml
-import logging
 
-logging.basicConfig(
-    format="[%(asctime)s] - %(levelname)s - %(message)s", level=logging.INFO
-)
+
+def is_valid_ipv4(address: str) -> bool:
+    # Regular expression for validating an IPv4 address
+    ipv4_regex = r"^(\d{1,3}\.){3}\d{1,3}$"
+
+    # Check if the address matches the IPv4 pattern
+    if re.match(ipv4_regex, address):
+        # Split the address into parts
+        parts = address.split(".")
+
+        # Check if there are 4 parts
+        if len(parts) == 4:
+            # Check the range of each part [0, 255]
+            for part in parts:
+                if not part.isdigit() or not 0 <= int(part) <= 255:
+                    return False
+            return True
+    return False
 
 
 if len(sys.argv) != 2:
@@ -15,13 +30,17 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 PUBLIC_IP = sys.argv[1]
+
+if not is_valid_ipv4(PUBLIC_IP) or PUBLIC_IP == "127.0.0.1":
+    print("Invalid IP address or localhost")
+    sys.exit(1)
 BASE_PORT = 1337
 
 TEMP = """
-container_name: chal-{chal}
 image: chal-{chal}
+container_name: chal-{chal}
 build:
-  context: {chal}/challenge/project
+  context: {chal}/challenge/
   dockerfile: Dockerfile.local
 command: socat TCP-LISTEN:1337,reuseaddr,fork exec:"python3 -u challenge/challenge.py"
 environment:
@@ -43,15 +62,16 @@ networks:
 
 chals = {}
 
-dirs = os.listdir(".")
+dirs = sorted(os.listdir("."))
 port = BASE_PORT
 for i, chal in enumerate(dirs):
     if (
-        os.path.isdir(chal)
+        chal != "infrastructure"
+        and os.path.isdir(chal)
         and os.path.exists(f"{chal}/challenge.yaml")
         and os.path.exists(f"{chal}/challenge/Dockerfile.local")
     ):
-        logging.info("Found challenge: '%s' with port: %d", chal, port)
+        print("Found challenge: '%s' with port: %d", chal, port)
         service = TEMP.format(chal=chal, port=port, PUBLIC_IP=PUBLIC_IP)
         chals[chal] = yaml.safe_load(service)
         port += 1
@@ -64,7 +84,7 @@ compose = {
     },
 }
 
-logging.info("Found %d challenges", len(chals))
-logging.info("Writing docker-compose.yml")
+print("Found %d challenges", len(chals))
+print("Writing docker-compose.yml")
 with open("docker-compose.yml", "w") as f:
     yaml.dump(compose, f, sort_keys=False)
